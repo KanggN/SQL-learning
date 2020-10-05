b91041588305439e0d90fd4cc1fab7f73f3b263c
KanggN <quangnguyenmail96@gmail.com>
2020-10-05 17:51:20 +0700

    add taphoa from visabli - self study

--- Media.sql
+++ Media.sql
@@ -23,6 +23,7 @@ insert into Categories values(1,'Phuong Thanh','Rock'),
 (3,'Lam Truong','Nhac Hoa'),
 (4,'Lam Truong','Nhac Viet'),
 (5,'Dan Truong','Hat voi Cam Ly')
+insert into Categories values(6,'Duy Manh','That tinh')
 insert into Album values(1,'PT1',1,'Anh PT1','Vol1',25000,1), 
 (2,'PT2',2,'Anh PT2','Vol2',30000,1), 
 (3,'LT1',3,'Anh LT1','Vol1',25000,1),
@@ -37,16 +38,16 @@ for delete
 as 
 	if exists (
 		select a.*
-		from Album a join Categories c on c.CateId = a.CateId)
+		from Album a join deleted d on a.CateId = d.CateId)
 	begin
-		print 'you cant'
+		print 'you cant delete category which has song(s) in it'
 		rollback tran
 	end
 
 select a.*
 from Album a join Categories c on c.CateId = a.CateId
  
-delete from Categories where CateID = 1
+delete from Categories where CateID = 4
 
 select * from Categories
 select * from Album
