8f00253a8c7fa5bbb6bc48ee86e1ef0ce61ad07c
KanggN <quangnguyenmail96@gmail.com>
2020-10-14 19:10:31 +0700

    add Sinhvien answers

--- SinhVien.sql
+++ SinhVien.sql
@@ -96,26 +96,26 @@ insert Lop values('TH2002/01','K2002','CNTT','CQ',1),
 ('TH2002/02','K2002','CNTT','CQ',2),
 ('VL2003/01','K2003','VL','CQ',1)
 
---Câu 1
+--Câu 4.1
 select sv.hoTen, k.tenKhoa
 from SinhVien sv join Lop l on sv.maLop = l.Ma join Khoa k on l.maKhoa = k.Ma
 where k.tenKhoa = N'Công nghệ thông tin'
---Câu 2
+--Câu 4.2
 select sv.hoTen,sv.namSinh,year(getdate()) - sv.namSinh age
 from SinhVien sv
 where year(getdate()) - sv.namSinh < 18
---Câu 3
+--Câu 4.3
 select sv.hoTen, kq.maMonHoc
 from SinhVien sv join Lop l on sv.maLop = l.Ma join KhoaHoc kh on kh.Ma = l.maKhoaHoc
 join KetQua kq on kq.maSinhVien = sv.Ma join MonHoc mh on mh.Ma = kq.maMonHoc
 where l.maKhoa = 'CNTT' and kh.namBatDau = 2002 and kh.namKetThuc = 2006 and mh.tenMonHoc = N'Cấu trúc dữ liệu 1'
 and sv.Ma not in (select maSinhVien from KetQua)
 group by sv.hoTen,kq.maMonHoc 
---Câu 4 
+--Câu 4.4 
 select sv.hoTen
 from SinhVien sv join KetQua kq on sv.Ma = kq.maSinhVien join MonHoc mh on mh.Ma = kq.maMonHoc
 where kq.diem < 5 and kq.lanThi = 1 and mh.tenMonHoc = N'Cấu trúc dữ liệu 1'
---Câu 5 
+--Câu 4.5 
 select sv.hoTen,sv.maLop, l.maKhoaHoc, ct.tenChuongTrinh, SL
 from SinhVien sv join Lop l on sv.maLop = l.Ma
 join ChuongTrinh ct on l.maChuongTrinh = ct.Ma
@@ -124,7 +124,7 @@ join SiSo s on s.maLop = sv.maLop
 select sv.maLop, count(sv.maLop) SL into SiSo --Temp Table
 from SinhVien sv
 group by sv.maLop
---Câu 6 
+--Câu 4.6 
 select avg(A.diem)--,A.maSinhVien,A.maMonHoc,A.diem,B.Lanthimax
 from KetQua A join (
 select distinct kq.maMonHoc,max(kq.lanThi) as Lanthimax
@@ -132,21 +132,82 @@ from KetQua kq
 where kq.maSinhVien = '0212003'
 group by kq.maMonHoc) B on A.maMonHoc = B.maMonHoc
 where A.lanThi = B.Lanthimax and A.maSinhVien = '0212003'
-
-alter proc kiemtra @hoten nvarchar(50), @ma varchar(20), @result char(1) output as --Cẩn thận nvarchar
+--Câu 5.1
+create proc Kiemtrakhoa @hoten nvarchar(50), @ma varchar(20), @result char(1) output as --Cẩn thận nvarchar input
 if exists(
-select *
-from SinhVien
-where hoTen = @hoten and Ma = @ma)
+select sv.*
+from SinhVien sv join lop l on l.Ma = sv.maLop
+where sv.hoTen = @hoten and l.maKhoa = @ma)
 	set @result = 'D'
 	else 
 	set @result = 'S'
-
 declare @a char(1)
-exec kiemtra N'Nguyễn Vĩnh An','0212001',@a output
+exec kiemtra N'Nguyễn Vĩnh An','VL',@a output
 print @a
-
-
+--Câu 5.2
+create proc Tradiem @Masv varchar(10), @Mon varchar(10), @diemthi int output as
+select @diemthi = kq.diem
+from KetQua kq join (select distinct kq.maMonHoc,max(kq.lanThi) as Lanthimax
+from KetQua kq
+where kq.maSinhVien = @Masv
+group by kq.maMonHoc
+) t on t.maMonHoc = kq.maMonHoc
+where kq.maSinhVien = @Masv and kq.maMonHoc = @Mon and kq.lanThi = t.Lanthimax
+declare @diem int
+exec Tradiem '0212003','THT02', @diem output
+print @diem
+--Câu 5.3
+alter proc Tradiemtb @Masv varchar(10), @Diemtb float output as
+select @Diemtb = avg(kq.diem)
+from KetQua kq join (select distinct kq.maMonHoc,max(kq.lanThi) as Lanthimax
+from KetQua kq
+where kq.maSinhVien = @Masv
+group by kq.maMonHoc
+) t on t.maMonHoc = kq.maMonHoc
+where kq.maSinhVien = @Masv and kq.lanThi = t.Lanthimax
+declare @diem float
+exec Tradiemtb '0212003', @diem output
+print @diem
+--Câu 5.4
+alter proc Tradiem1 @Masv varchar(10), @Mon varchar(10) as
+select kq.lanThi,kq.diem
+from KetQua kq 
+where kq.maSinhVien = @Masv and kq.maMonHoc = @Mon 
+exec Tradiem1 '0212003','THT01'
+--Câu 5.5
+create proc Tramonhoc @Masv varchar(10) as
+select mh.tenMonHoc
+from SinhVien sv join lop l on l.Ma = sv.maLop
+join MonHoc mh on mh.maKhoa = l.maKhoa
+where sv.Ma = @Masv
+exec Tramonhoc '0311002'
+--Câu 6.1
+create proc Tralop @Malop varchar (20) as
+select *
+from SinhVien
+where maLop = @Malop
+exec Tralop 'TH2002/01'
+--Câu 6.2
+alter proc Sodiem @Masv varchar(10),@Masv1 varchar(10), @Mon varchar(20) as
+declare @diem float, @diem1 float
+select @diem = kq.diem
+from KetQua kq 
+where maSinhVien = @Masv
+and maMonHoc = @Mon and lanThi = 1 
+select @diem1 = kq.diem
+from KetQua kq 
+where maSinhVien = @Masv1
+and maMonHoc = @Mon and lanThi = 1
+If (@diem > @diem1)
+	print N'SV thứ nhất cao điểm hơn (' + CAST(@diem as varchar) + N' điểm so với ' + CAST(@diem1 as varchar) + N' điểm)'
+	else if (@diem = @diem1)
+	print N'Điểm 2 SV bằng nhau (' + CAST(@diem as varchar) + N' điểm'
+	else
+	print N'SV thứ hai cao điểm hơn (' + CAST(@diem1 as varchar) + N' điểm so với ' + CAST(@diem as varchar) + N' điểm)'
+exec Sodiem '0212002','0212001','THT02'
+select *
+from KetQua
+where maMonHoc = 'VLT01'
 
 select * from SiSo
 select * from SinhVien
